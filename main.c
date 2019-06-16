875a5b06dbe7e2269cb8b96d4b317449715da118
Baytuch <dev@it-hobby.km.ua>
2019-06-16 13:45:31 +0200

    Finished base functionality

--- main.c
+++ main.c
@@ -10,6 +10,9 @@
 
 struct LED strip[STRIP_LEN];
 
+static struct LED led_target;
+static uint8_t strip_police_n;
+
 void strip_run(struct LED *data) {
   uint8_t i = STRIP_LEN;
   while (i > 0) {
@@ -23,8 +26,7 @@ void strip_run(struct LED *data) {
   data[0].b = (uint8_t) rand() % STRIP_LIGHT;
 }
 
-static struct LED led_smooth_target;
-void strip_run_smooth(struct LED *data) {
+void strip_hypnos(struct LED *data) {
   uint8_t i = STRIP_LEN;
   while (i > 0) {
     i--;
@@ -32,20 +34,19 @@ void strip_run_smooth(struct LED *data) {
     data[i + 1].g = data[i].g;
     data[i + 1].b = data[i].b;
   }
-  if (data[0].r > led_smooth_target.r) data[0].r--;
-  if (data[0].r < led_smooth_target.r) data[0].r++;
-  if (data[0].g > led_smooth_target.g) data[0].g--;
-  if (data[0].g < led_smooth_target.g) data[0].g++;
-  if (data[0].b > led_smooth_target.b) data[0].b--;
-  if (data[0].b < led_smooth_target.b) data[0].b++;
-  if (data[0].r == led_smooth_target.r && data[0].g == led_smooth_target.g && data[0].b == led_smooth_target.b) {
-    led_smooth_target.r = (uint8_t) rand() % STRIP_LIGHT;
-    led_smooth_target.g = (uint8_t) rand() % STRIP_LIGHT;
-    led_smooth_target.b = (uint8_t) rand() % STRIP_LIGHT;
+  if (data[0].r > led_target.r) data[0].r--;
+  if (data[0].r < led_target.r) data[0].r++;
+  if (data[0].g > led_target.g) data[0].g--;
+  if (data[0].g < led_target.g) data[0].g++;
+  if (data[0].b > led_target.b) data[0].b--;
+  if (data[0].b < led_target.b) data[0].b++;
+  if (data[0].r == led_target.r && data[0].g == led_target.g && data[0].b == led_target.b) {
+    led_target.r = (uint8_t) rand() % STRIP_LIGHT;
+    led_target.g = (uint8_t) rand() % STRIP_LIGHT;
+    led_target.b = (uint8_t) rand() % STRIP_LIGHT;
   }
 }
 
-static uint8_t strip_police_n;
 void strip_police(struct LED *data) {
   for (uint8_t i = 0; i < STRIP_LEN; i++) {
     if (i <= STRIP_LEN / 2 - 3 && (strip_police_n == 0 || strip_police_n == 2)) {
@@ -66,7 +67,6 @@ void strip_police(struct LED *data) {
   if (strip_police_n > 10) strip_police_n = 0;
 }
 
-static uint8_t strip_police_n;
 void strip_police_dobs(struct LED *data) {
   for (uint8_t i = 0; i < STRIP_LEN; i++) {
     if (i <= STRIP_LEN / 2 - 3) {
@@ -94,6 +94,7 @@ void strip_police_dobs(struct LED *data) {
   if (strip_police_n > STRIP_LEN / 2 - 3) strip_police_n = 0;
 }  
 
+static uint16_t main_n = 0;
 int main(void) {
 
   ws2812_setup();
@@ -106,9 +107,16 @@ int main(void) {
 
   while(1){
 
-    strip_run_smooth(strip);
+    strip_hypnos(strip);
+
+    if (main_n < 300) strip_police(strip);
+    else if (main_n >= 300 && main_n <= 600) strip_police_dobs(strip);
+    else strip_hypnos(strip);
 
     ws2812_send(strip, STRIP_LEN);
+
+    main_n++;
+    if (main_n > 1000) main_n = 0;
 
     ws2812_wait();
   }
